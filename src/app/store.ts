bf3106ed6f176c1faa83bf00a4dbd493cfb71410
beebeeoii <b33b33o11@gmail.com>
2021-12-16 14:36:04 +0800

    persistent redux

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,17 +2,40 @@ import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 // import counterReducer from '../features/counter/counterSlice';
 import authReducer from '../services/auth/authSplice';
 
+const saveToLocalStorage = (state: object) => {
+    try {
+        localStorage.setItem('state', JSON.stringify(state))
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+const loadFromLocalStorage = () => {
+    try {
+        const stateStr = localStorage.getItem('state')
+        return stateStr ? JSON.parse(stateStr) : undefined
+    } catch (e) {
+        console.error(e)
+        return undefined
+    }
+}
+
 export const store = configureStore({
     reducer: {
-        auth: authReducer,
+        auth: authReducer
     },
-});
+    preloadedState: loadFromLocalStorage()
+})
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
+store.subscribe(() => {
+    saveToLocalStorage(store.getState())
+})
+
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
 export type AppThunk<ReturnType = void> = ThunkAction<
     ReturnType,
     RootState,
     unknown,
     Action<string>
->;
+>
