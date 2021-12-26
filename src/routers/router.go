81116decdb613c7bdb9d236b8419a88448a120f3
beebeeoii <b33b33o11@gmail.com>
2021-12-26 22:07:24 +0800

    added cors middleware

--- src/routers/router.go
+++ src/routers/router.go
@@ -9,9 +9,27 @@ import (
 
 func Init(address string) {
 	router := gin.Default()
+	router.Use(CORSMiddleware())
 	router.GET("/user/authenticate", auth.AuthenticateUser)
 	router.POST("/user", user.Register)
 	router.GET("/user/:id", user.RetrieveUserById)
 
 	router.Run(address)
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, id")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
