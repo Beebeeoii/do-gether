5e51d8d68415b51ef2bf4dca0cca9c09327de4cd
beebeeoii <b33b33o11@gmail.com>
2022-01-03 21:29:35 +0800

    added friends endpoints

--- src-ui/src/adapters/user/user.tsx
+++ src-ui/src/adapters/user/user.tsx
@@ -1,5 +1,5 @@
 import { AuthData } from "../../interfaces/auth/Auth";
-import { sendGet } from "../adapter";
+import { sendDelete, sendGet, sendPost } from "../adapter";
 
 export function fetchUserInfo(authData: AuthData, userId: string) {
     let headers = {
@@ -8,4 +8,68 @@ export function fetchUserInfo(authData: AuthData, userId: string) {
     }
 
     return sendGet(`/user/${userId}`, undefined, headers)
+}
+
+export function fetchUserByUsername(authData: AuthData, username: string) {
+    let headers = {
+        "Authorization": `Bearer ${authData.token}`,
+        "id": authData.id
+    }
+
+    let params = {
+        "username": username
+    }
+
+    return sendGet(`/user/friend`, params, headers)
+}
+
+export function sendOutgoingFriendRequest(authData: AuthData, userId: string) {
+    let headers = {
+        "Authorization": `Bearer ${authData.token}`,
+        "id": authData.id
+    }
+
+    let body = {
+        "id": userId
+    }
+
+    return sendPost(`/user/friend/sendReq`, body, headers)
+}
+export function deleteFriendRequest(authData: AuthData, userId: string) {
+    let headers = {
+        "Authorization": `Bearer ${authData.token}`,
+        "id": authData.id
+    }
+
+    let params = {
+        "id": userId
+    }
+
+    return sendDelete(`/user/friend/deleteReq`, params, headers)
+}
+
+export function acceptIncomingFriendRequest(authData: AuthData, userId: string) {
+    let headers = {
+        "Authorization": `Bearer ${authData.token}`,
+        "id": authData.id
+    }
+
+    let body = {
+        "id": userId
+    }
+
+    return sendPost(`/user/friend/acceptReq`, body, headers)
+}
+
+export function deleteFriend(authData: AuthData, userId: string) {
+    let headers = {
+        "Authorization": `Bearer ${authData.token}`,
+        "id": authData.id
+    }
+
+    let params = {
+        "id": userId
+    }
+
+    return sendDelete(`/user/friend`, params, headers)
 }
