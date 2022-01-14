5a658978107ade3794e7f54e52a00442e8529803
beebeeoii <b33b33o11@gmail.com>
2022-01-14 19:59:29 +0800

    responsive login and register pages

--- src-ui/src/pages/login/Login.tsx
+++ src-ui/src/pages/login/Login.tsx
@@ -1,4 +1,4 @@
-import { Alert, Button, Card, Link, Snackbar, Stack, TextField, Typography } from "@mui/material"
+import { Alert, Button, Card, Container, Link, Snackbar, Stack, TextField, Typography } from "@mui/material"
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAppDispatch } from "../../app/hooks";
@@ -6,7 +6,6 @@ import { LoginResponse } from "../../interfaces/auth/AuthResponse";
 import { Credentials } from "../../interfaces/auth/Credentials";
 import { SnackBarState } from "../../interfaces/utils/Snackbar";
 import { login } from "../../services/auth/authSplice";
-import "./Login.css"
 
 export function Login() {
     const defaultSnackBarState: SnackBarState = {
@@ -33,51 +32,52 @@ export function Login() {
     const [password, setPassword] = useState('')
 
     return (
-        <Stack direction={"column"} marginTop={24} sx={{ width: "22rem" }}>
-            <Card className="login" elevation={3}>
-                <Stack direction={"column"} gap={3} sx={{ padding: "2rem 2.5rem" }}>
-                    <Typography variant="h5" sx={{ fontWeight: "bold", marginBottom: "1rem" }}>
-                        Log in to Do-gether
-                    </Typography>
+        <Container maxWidth="xs">
+            <Stack direction={"column"} marginTop={"8rem"}>
+                <Card className="login" elevation={3}>
+                    <Stack direction={"column"} gap={3} sx={{ padding: "2rem 2.5rem" }}>
+                        <Typography variant="h5" sx={{ fontWeight: "bold", marginBottom: "1rem" }}>
+                            Log in to Do-gether
+                        </Typography>
 
-                    <TextField id="usernameInput" label="Username" variant="outlined" onChange={(e) => setUsername(e.target.value)} autoFocus />
-                    <TextField id="passwordInput" label="Password" variant="outlined" type="password" autoComplete="current-password" onChange={(e) => setPassword(e.target.value)} />
-                    <Button variant="contained" onClick={async () => {
-                        let credentials: Credentials = {
-                            username: username,
-                            password: password
-                        }
-                        let authRes = await dispatch(login(credentials))
-                        let authPayload = authRes.payload as LoginResponse
+                        <TextField id="usernameInput" label="Username" variant="outlined" onChange={(e) => setUsername(e.target.value)} autoFocus />
+                        <TextField id="passwordInput" label="Password" variant="outlined" type="password" autoComplete="current-password" onChange={(e) => setPassword(e.target.value)} />
+                        <Button variant="contained" onClick={async () => {
+                            let credentials: Credentials = {
+                                username: username,
+                                password: password
+                            }
+                            let authRes = await dispatch(login(credentials))
+                            let authPayload = authRes.payload as LoginResponse
 
-                        if (authPayload.success) {
-                            navigate("/dashboard")
-                        } else {
-                            openSnackBar({
-                                open: true,
-                                severity: "error",
-                                message: `Error: ${authPayload.error}`
-                            })()
-                        }
-                    }}>
-                        Login
-                    </Button>
+                            if (authPayload.success) {
+                                navigate("/dashboard")
+                            } else {
+                                openSnackBar({
+                                    open: true,
+                                    severity: "error",
+                                    message: `Error: ${authPayload.error}`
+                                })()
+                            }
+                        }}>
+                            Login
+                        </Button>
 
-                    <Snackbar open={snackBarState.open} autoHideDuration={6000} onClose={closeSnackBar} anchorOrigin={{ vertical: "bottom", horizontal: "center" }}>
-                        <Alert onClose={closeSnackBar} severity={snackBarState.severity} sx={{ width: '100%' }}>
-                            {snackBarState.message}
-                        </Alert>
-                    </Snackbar>
-                </Stack>
-            </Card>
+                        <Snackbar open={snackBarState.open} autoHideDuration={6000} onClose={closeSnackBar} anchorOrigin={{ vertical: "bottom", horizontal: "center" }}>
+                            <Alert onClose={closeSnackBar} severity={snackBarState.severity} sx={{ width: '100%' }}>
+                                {snackBarState.message}
+                            </Alert>
+                        </Snackbar>
+                    </Stack>
+                </Card>
 
-            <Typography variant="caption" sx={{ marginTop: "1rem", marginLeft: "0.5rem" }}>
-                No account?
-                <Link href="/register" sx={{ marginLeft: "0.3rem" }}>
-                    Register
-                </Link>
-            </Typography>
-        </Stack>
-
+                <Typography variant="caption" sx={{ marginTop: "1rem", marginLeft: "0.5rem" }}>
+                    No account?
+                    <Link href="/register" sx={{ marginLeft: "0.3rem" }}>
+                        Register
+                    </Link>
+                </Typography>
+            </Stack>
+        </Container>
     )
 }
--- src-ui/src/pages/register/Register.tsx
+++ src-ui/src/pages/register/Register.tsx
@@ -1,4 +1,4 @@
-import { Alert, Button, Card, Link, Snackbar, Stack, TextField, Typography } from "@mui/material"
+import { Alert, Button, Card, Container, Link, Snackbar, Stack, TextField, Typography } from "@mui/material"
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { useAppDispatch } from "../../app/hooks"
@@ -6,7 +6,6 @@ import { LoginResponse } from "../../interfaces/auth/AuthResponse"
 import { Credentials } from "../../interfaces/auth/Credentials"
 import { SnackBarState } from "../../interfaces/utils/Snackbar"
 import { register } from "../../services/auth/authSplice"
-import "./Register.css"
 
 export function Register() {
     const defaultSnackBarState: SnackBarState = {
@@ -33,54 +32,55 @@ export function Register() {
     const [password, setPassword] = useState('')
 
     return (
-        <Stack direction={"column"} marginTop={24} sx={{ width: "22rem" }}>
-            <Card className="register">
-                <Stack direction={"column"} gap={3} sx={{ padding: "2rem 2.5rem" }}>
-                    <Typography variant="h5" sx={{ fontWeight: "bold", marginBottom: "1rem" }}>
-                        Get started with
-                        <br/>
-                        Do-gether
-                    </Typography>
+        <Container maxWidth="xs">
+            <Stack direction={"column"} marginTop={"8rem"}>
+                <Card className="register">
+                    <Stack direction={"column"} gap={3} sx={{ padding: "2rem 2.5rem" }}>
+                        <Typography variant="h5" sx={{ fontWeight: "bold", marginBottom: "1rem" }}>
+                            Get started with
+                            <br />
+                            Do-gether
+                        </Typography>
 
-                    <TextField id="usernameInput" label="Username" variant="outlined" onChange={(e) => setUsername(e.target.value)} autoFocus />
-                    <TextField id="passwordInput" label="Password" variant="outlined" type="password" autoComplete="current-password" onChange={(e) => setPassword(e.target.value)} />
-                    <Button variant="contained" onClick={async () => {
-                        let credentials: Credentials = {
-                            username: username,
-                            password: password
-                        }
-                        let authRes = await dispatch(register(credentials))
-                        let authPayload = authRes.payload as LoginResponse
+                        <TextField id="usernameInput" label="Username" variant="outlined" onChange={(e) => setUsername(e.target.value)} autoFocus />
+                        <TextField id="passwordInput" label="Password" variant="outlined" type="password" autoComplete="current-password" onChange={(e) => setPassword(e.target.value)} />
+                        <Button variant="contained" onClick={async () => {
+                            let credentials: Credentials = {
+                                username: username,
+                                password: password
+                            }
+                            let authRes = await dispatch(register(credentials))
+                            let authPayload = authRes.payload as LoginResponse
 
-                        if (authPayload.success) {
-                            navigate("/dashboard")
-                        } else {
-                            openSnackBar({
-                                open: true,
-                                severity: "error",
-                                message: `Error: ${authPayload.error}`
-                            })()
-                        }
-                    }}>
-                        Register
-                    </Button>
+                            if (authPayload.success) {
+                                navigate("/dashboard")
+                            } else {
+                                openSnackBar({
+                                    open: true,
+                                    severity: "error",
+                                    message: `Error: ${authPayload.error}`
+                                })()
+                            }
+                        }}>
+                            Register
+                        </Button>
 
-                    <Snackbar open={snackBarState.open} autoHideDuration={6000} onClose={closeSnackBar} anchorOrigin={{ vertical: "bottom", horizontal: "center" }}>
-                        <Alert onClose={closeSnackBar} severity={snackBarState.severity} sx={{ width: '100%' }}>
-                            {snackBarState.message}
-                        </Alert>
-                    </Snackbar>
-                </Stack>
+                        <Snackbar open={snackBarState.open} autoHideDuration={6000} onClose={closeSnackBar} anchorOrigin={{ vertical: "bottom", horizontal: "center" }}>
+                            <Alert onClose={closeSnackBar} severity={snackBarState.severity} sx={{ width: '100%' }}>
+                                {snackBarState.message}
+                            </Alert>
+                        </Snackbar>
+                    </Stack>
 
-            </Card>
+                </Card>
 
-            <Typography variant="caption" sx={{ marginTop: "1rem", marginLeft: "0.5rem" }}>
-                Already have an account?
-                <Link href="/login" sx={{ marginLeft: "0.3rem" }}>
-                    Login
-                </Link>
-            </Typography>
-        </Stack >
-
+                <Typography variant="caption" sx={{ marginTop: "1rem", marginLeft: "0.5rem" }}>
+                    Already have an account?
+                    <Link href="/login" sx={{ marginLeft: "0.3rem" }}>
+                        Login
+                    </Link>
+                </Typography>
+            </Stack >
+        </Container>
     )
 }
