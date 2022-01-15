bb4ba411db9265efe98a5b806642e56ff2410fee
beebeeoii <b33b33o11@gmail.com>
2022-01-15 16:12:50 +0800

    updated port to be in env

--- src/main.go
+++ src/main.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/beebeeoii/do-gether/db"
 	router "github.com/beebeeoii/do-gether/routers"
 	"github.com/joho/godotenv"
-)
-
-const (
-	serverAddress = "0.0.0.0:8080"
 )
 
 func main() {
@@ -23,5 +20,5 @@ func main() {
 		log.Fatalln(psqlDbErr)
 	}
 
-	router.Init(serverAddress)
+	router.Init(os.Getenv("SERVER_ADD"))
 }
