8ba5432f7d1003252bfb3d3291bfd537cd239af2
beebeeoii <b33b33o11@gmail.com>
2022-01-15 20:02:38 +0800

    bugfix: table column names not camelcase

--- src-psql/CreateTasksTable.sql
+++ src-psql/CreateTasksTable.sql
@@ -3,11 +3,11 @@ CREATE TABLE tasks (
     owner VARCHAR(20) NOT NULL,
     title TEXT NOT NULL,
     tags VARCHAR(20)[] NOT NULL,
-    listId VARCHAR(20) NOT NULL,
-    listOrder INTEGER NOT NULL,
+    "listId" VARCHAR(20) NOT NULL,
+    "listOrder" INTEGER NOT NULL,
     priority SMALLINT NOT NULL,
     due BIGINT NOT NULL,
-    plannedStart BIGINT NOT NULL,
-    plannedEnd BIGINT NOT NULL,
+    "plannedStart" BIGINT NOT NULL,
+    "plannedEnd" BIGINT NOT NULL,
     completed BOOLEAN NOT NULL
 );
