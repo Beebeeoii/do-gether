f3b1b15e18246541debf0bb1e59eb03ac94f1315
beebeeoii <b33b33o11@gmail.com>
2022-01-15 18:32:38 +0800

    added connection retry logic

--- src/db/psql.go
+++ src/db/psql.go
@@ -6,13 +6,16 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"time"
 
 	_ "github.com/lib/pq"
 )
 
 const (
-	DB_DRIVER = "postgres"
-	SSL_MODE  = "disable"
+	DB_DRIVER      = "postgres"
+	SSL_MODE       = "disable"
+	RETRY_ATTEMPTS = 5
+	SLEEP_SECONDS  = 5
 )
 
 var Database *sql.DB
@@ -30,7 +33,18 @@ func Init() (err error) {
 	log.Println(POSTGRES_HOST, POSTGRES_PASSWORD, POSTGRES_PORT, POSTGRES_USER)
 
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s", POSTGRES_HOST, POSTGRES_PORT, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, SSL_MODE)
-	return connect(psqlInfo)
+
+	for i := 0; i < RETRY_ATTEMPTS; i++ {
+		connectionErr := connect(psqlInfo)
+		if connectionErr != nil {
+			log.Printf("attempt %d: retrying psql db connection after %d seconds\n", i, SLEEP_SECONDS)
+			time.Sleep(SLEEP_SECONDS * time.Second)
+		} else {
+			return nil
+		}
+	}
+
+	return fmt.Errorf("unable to connect to psql db after %d attempts", RETRY_ATTEMPTS)
 }
 
 func connect(psqlInfo string) (err error) {
